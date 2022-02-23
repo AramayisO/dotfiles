e44644b18f71940a779dc16a137d2fc5210830cb
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2022-02-22 17:41:40 -0800

    Configure nvim-tree

--- nvim/.config/nvim/lua/setup/nvim-tree.lua
+++ nvim/.config/nvim/lua/setup/nvim-tree.lua
@@ -1,10 +1,76 @@
+-- Global options must be set before calling `require'nvim-tree'` or calling setup
+vim.cmd[[let g:nvim_tree_quit_on_open = 1]]
+vim.cmd[[let g:nvim_tree_indent_markers = 1]]
+vim.cmd[[let g:nvim_tree_git_hl = 1]]
+vim.cmd[[let g:nvim_tree_highlight_opened_files = 1]]
+vim.cmd[[let g:nvim_tree_root_folder_modifier = ':~']]
+vim.cmd[[let g:nvim_tree_add_trailing = 1]]
+vim.cmd[[let g:nvim_tree_group_empty = 1]]
+vim.cmd[[let g:nvim_tree_disable_window_picker = 1]]
+vim.cmd[[let g:nvim_tree_icon_padding = ' ']]
+vim.cmd[[let g:nvim_tree_symlink_arrow = ' >> ']]
+vim.cmd[[let g:nvim_tree_respect_buf_cwd = 1]]
+vim.cmd[[let g:nvim_tree_create_in_closed_folder = 1]]
+vim.cmd[[let g:nvim_tree_refresh_wait = 500]]
+vim.cmd[[let g:nvim_tree_window_picker_exclude = {
+    \   'filetype': [
+    \     'notify',
+    \     'packer',
+    \     'qf'
+    \   ],
+    \   'buftype': [
+    \     'terminal'
+    \   ]
+    \ }
+]]
+vim.cmd[[let g:nvim_tree_special_files = { 'README.md': 1, 'Makefile': 1, 'MAKEFILE': 1 }]]
+vim.cmd[[
+    let g:nvim_tree_show_icons = {
+        \ 'git': 1,
+        \ 'folders': 1,
+        \ 'files': 1,
+        \ 'folder_arrows': 1,
+        \ }
+]]
+vim.cmd[[
+  let g:nvim_tree_icons = {
+      \ 'default':        '',
+      \ 'symlink':        '',
+      \ 'git': {
+      \   'unstaged':     "✗",
+      \   'staged':       "",
+      \   'unmerged':     "",
+      \   'renamed':      "➜",
+      \   'untracked':    "★",
+      \   'deleted':      "",
+      \   'ignored':      "◌",
+      \  },
+      \ 'folder': {
+      \   'arrow_open':   "",
+      \   'arrow_closed': "",
+      \   'default':      "",
+      \   'open':         "",
+      \   'empty':        "",
+      \   'empty_open':   "",
+      \   'symlink':      "",
+      \   'symlink_open': "",
+      \  },
+      \  'lsp': {
+      \    'hint': "",
+      \    'info': "",
+      \    'warning': "",
+      \    'error': "",
+      \  }
+      \ }
+]]
+
 -- Each of these options is documented in `:help nvim-tree.OPTION_NAME`
 require'nvim-tree'.setup {
     disable_netrw        = false,
     hijack_netrw         = true,
     open_on_setup        = false,
     ignore_ft_on_setup   = {},
-    auto_close           = false,
+    auto_close           = true,
     auto_reload_on_write = true,
     open_on_tab          = false,
     hijack_cursor        = false,
