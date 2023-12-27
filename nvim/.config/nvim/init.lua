e5941e23f6115383425cd82ee53c2dfffae7292f
Aramayis Orkusyan <aramayis@baton.io>
2023-12-26 23:27:26 -0800

    Bootstrap lazy.nvim for plugin management

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -33,3 +33,18 @@ vim.o.splitright = true
 
 -- Highlight line
 vim.opt.cursorline = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({}, {})
