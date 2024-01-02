046170912d65d47f22d23109c51f9a2cdbc8cbe7
Aramayis Orkusyan <aramayis@baton.io>
2024-01-02 15:08:14 -0800

    Set tokyonight style to night

---
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+    styles = {
+      sidebars = "dark", -- style for sidebars, see below
+      floats = "dark", -- style for floating windows
+    },
+  },
+}
