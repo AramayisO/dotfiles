7bdb3475340d01e0add2af1c8e115d542e0bbf96
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2024-09-02 16:55:16 -0700

    Move configs out of init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,16 +1,8 @@
 require("config.lazy")
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
 
-print("Hello lua")
 
-
--- Set tabs to 4 spaces
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-
--- Auto indent new lines
-vim.opt.autoindent = true
-vim.opt.smartindent = true
 
 
---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Theme
+vim.cmd("colorscheme cyberdream")
+
+-- Set tabs to 4 spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Auto indent new lines
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Show line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
