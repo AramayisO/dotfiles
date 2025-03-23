12c6ebdfc72c9dd458079b5c7d98c8bad111479c
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2025-03-23 12:12:15 -0700

    nvim: set default split directions to below and right

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,5 +1,11 @@
+-- Tabs to spaces
 local TAB_SPACES = 4
 vim.opt.tabstop = TAB_SPACES 
 vim.opt.softtabstop = TAB_SPACES
 vim.opt.shiftwidth = TAB_SPACES
 vim.opt.expandtab = true
+
+-- Default split directions
+vim.opt.splitbelow = true 
+vim.opt.splitright = true 
+
