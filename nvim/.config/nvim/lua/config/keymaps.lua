a228a747870483905bd7d9dcfd56d77f037d2f2a
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2025-03-23 20:34:24 -0700

    nvim: add basic telescope keymaps

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+local telescope_builtins = require("telescope.builtin")
+
+-- Telescope keymaps
+vim.keymap.set("n", "<leader>ff", telescope_builtins.find_files, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fg", telescope_builtins.live_grep, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fb", telescope_builtins.buffers, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fh", telescope_builtins.help_tags, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fs", telescope_builtins.grep_string, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>/", telescope_builtins.current_buffer_fuzzy_find, { noremap = true, silent = true })
