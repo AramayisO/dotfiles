3d338ae9d67b12eb2525714ad1925e84b603975b
Aramayis Orkusyan <aramayis@baton.io>
2025-03-29 12:27:58 -0700

    nvim: add blink.cmp for code completion

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,51 +1,61 @@
-return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local lspconfig = require("lspconfig")
+local lua_ls_config = {
+    on_init = function (client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if path ~= vim.fn.stdpath("config") and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc")) then
+                return
+            end
+        end
 
-        lspconfig.lua_ls.setup({
-            on_init = function(client)
-                if client.workspace_folders then
-                    local path = client.workspace_folders[1].name
-                    if
-                        path ~= vim.fn.stdpath("config")
-                        and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
-                    then
-                        return
-                    end
-                end
-
-                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using
-                        -- (most likely LuaJIT in the case of Neovim)
-                        version = "LuaJIT",
-                    },
-                    -- Make the server aware of Neovim runtime files
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME,
-                            -- Depending on the usage, you might want to add additional paths here.
-                            -- "${3rd}/luv/library"
-                            -- "${3rd}/busted/library",
-                        },
-                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-                        -- library = vim.api.nvim_get_runtime_file("", true)
-                    },
-                })
-            end,
-            settings = {
-                Lua = {},
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+            runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    -- Depending on the usage, you might want to add additional paths here.
+                    -- "${3rd}/luv/library"
+                    -- "${3rd}/busted/library",
+                },
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                -- library = vim.api.nvim_get_runtime_file("", true)
             },
         })
-
-        lspconfig.gopls.setup({})
-
-        lspconfig.pyright.setup({
-            on_new_config = function(config, root)
-                config.settings.python.pythonPath = vim.fn.expand(root .. "/.venv/bin/python")
-            end,
-        })
     end,
+
+    settings = {
+        Lua = {},
+    }
+}
+
+local pyright_config = {
+    on_new_config = function (config, root)
+        config.settings.python.pythonPath = vim.fn.expand(root .. "/.venv/bin/python")
+    end
+}
+
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = { 'saghen/blink.cmp' },
+    opts = {
+        servers = {
+            lua_ls = lua_ls_config,
+            pyright = pyright_config,
+            gopls = {},
+        },
+    },
+    config = function (_, opts)
+        local lspconfig = require("lspconfig")
+        -- setup each lsp in opts.servers
+        -- pass config.capabilities to blink.cmp for each to merge capabilities
+        for server, config in pairs(opts.servers) do
+            config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+            lspconfig[server].setup(config)
+        end
+    end
 }
